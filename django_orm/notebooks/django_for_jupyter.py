68a199095eac64014a27bc24c67deece0d46d9e1
BenedictusAryo <benedictusaryo@outlook.com>
2021-02-02 22:23:24 +0700

    :white_check_mark: Modify Django modules for jupyter

--- django_orm/notebooks/django_for_jupyter.py
+++ django_orm/notebooks/django_for_jupyter.py
@@ -4,7 +4,8 @@ Modules to using Django ORM in Jupyter Notebooks
 Credit: https://kirr.co/xckxq3
 """
 import os, sys
-PWD = os.getenv('PWD')
+PWD = os.path.join(os.getcwd(), os.pardir)
+DJ_DIR = os.path.abspath(PWD)
 
 PROJ_MISSING_MSG = """Set an enviroment variable:\n
 `DJANGO_PROJECT=your_project_name`\n
@@ -13,12 +14,13 @@ or call:\n
 """
 
 def init_django(project_name=None):
-    os.chdir(PWD)
+    print("Django Project dir: ",DJ_DIR)
+    os.chdir(DJ_DIR)
     project_name = project_name or os.environ.get('DJANGO_PROJECT') or None
     if project_name == None:
         raise Exception(PROJ_MISSING_MSG)
     sys.path.insert(0, os.getenv('PWD'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{project_name}.settings')
-    os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = "true"
+    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
     import django
     django.setup()
